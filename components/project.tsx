c748ffe5da8442c7e414431f13bb18ed7b6a3d0b
Sean Yak <compacodes@gmail.com>
2024-07-08 08:39:37 -0400

    edit skills

--- components/project.tsx
+++ components/project.tsx
@@ -54,21 +54,22 @@ export default function Project({
           </div>
           <Image
             src={imageUrl}
-            alt="Project I worked on"
+            alt={title}
             quality={95}
             className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
-          transition 
-          group-hover:scale-[1.04]
-          group-hover:-translate-x-3
-          group-hover:translate-y-3
-          group-hover:-rotate-2
+              transition 
+              group-hover:scale-[1.04]
+              group-hover:-translate-x-3
+              group-hover:translate-y-3
+              group-hover:-rotate-2
+           
 
-          group-even:group-hover:translate-x-3
-          group-even:group-hover:translate-y-3
-          group-even:group-hover:rotate-2
-
-          group-even:right-[initial] group-even:-left-40"
-          />
+              group-even:group-hover:translate-x-3  
+              group-even:group-hover:translate-y-3
+              group-even:group-hover:rotate-2
+              group-even:right-[initial] 
+              group-even:-left-40"
+              />
         </Link>
       </section>
     </motion.div>
