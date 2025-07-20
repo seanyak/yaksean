650cf0b755e6449d985465acbe19e7b1a95fc288
Sean Yak <compacodes@gmail.com>
2025-07-20 09:39:47 -0400

    upsertSkills

--- components/about.tsx
+++ components/about.tsx
@@ -22,13 +22,13 @@ export default function About() {
         focused on design and development for the music industry, crafting logos, flyers, album art, 
         merch, and full websites.
       </p>
-      <p>
+      <p className="mb-3 text-justify">
         Nowadays I’m a freelance web developer, graphic designer, and entrepreneur with a core tech 
         stack of{" "}<span className="font-medium">React, Next.js, TypeScript, Tailwind, and 
         MongoDB</span>. I’ve been working independently since 2021 and I’m always eager to learn, 
         build, and level up.
       </p>
-      <p>
+      <p className="mb-3 text-justify">
         When I’m not designing and developing, I’m making music, digging for new sounds, coaching youth soccer, or hanging at the beach with the fam.
       </p>
       <p className="mt-3 text-xl font-medium">
