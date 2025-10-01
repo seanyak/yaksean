98780d169630b011b8cfd894f3897b4d09178748
Sean Yak <compacodes@gmail.com>
2025-10-01 08:58:26 -0400

    upsert data

--- components/about.tsx
+++ components/about.tsx
@@ -17,14 +17,11 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3 text-justify">
-        Native of CA, born in L.A., raised in Orange County, now rooted in Wilmington, NC. I started off
-        designing graphics and building websites for local bands and friends in the late '90s . Since then, 
-        my focus has been in the music industry, crafting logos, flyers, album art, merch, and websites.
-      </p>
-      <p className="mb-3 text-justify">
+        I started off designing graphics and building websites for local bands and friends in the late '90s. 
+        Since then, my focus has been in the music industry, crafting logos, flyers, album art, merch, and websites.
         Nowadays I’m a freelance web developer, graphic designer, and entrepreneur with a core tech 
         stack of{" "}<span className="font-medium">React, Next.js, TypeScript, Tailwind, and 
-        MongoDB</span>. I’ve been working independently since 2021 and I’m always eager to learn, 
+        MongoDB</span>. I’ve been working independently since 2021. Always eager to learn, 
         build, and level up.
       </p>
       <p className="mt-3 text-xl font-medium py-7">
