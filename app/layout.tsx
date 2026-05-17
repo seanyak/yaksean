983d31025e4bfbe07fe22bf187817ce83e080a93
Sean Yak <compacodes@gmail.com>
2026-05-17 12:42:05 -0400

    upsert data

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,8 +75,8 @@ export default function RootLayout({
     <html lang="en" className="!scroll-smooth">
       <body 
         className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}>
-        <div className='bg-[#fefce8] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-none lg:blur-[29rem] sm:w-[68.75rem] dark:bg-[#022c22]'></div>
-        <div className='bg-[#bae6fd] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-none lg:blur-[29rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#0c4a6e]'></div>        <ThemeContextProvider>
+        <div className='hidden lg:block bg-[#fefce8] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-none lg:blur-[29rem] sm:w-[68.75rem] dark:bg-[#022c22]'></div>
+        <div className='hidden lg:block bg-[#bae6fd] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-none lg:blur-[29rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#0c4a6e]'></div>        <ThemeContextProvider>
           <ActiveSectionContextProvider>
             <Header />
             {children}
